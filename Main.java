950df162b0823ddccc893eb4ce30c673fa1d56cd
mayannyda123 <capic.2024118isinf0058@aluno.ifpi.edu.br>
2025-06-11 07:56:59 -0300

    pessoa.java criada ahahahaha

--- Main.java
+++ Main.java
@@ -2,6 +2,10 @@
 public class Main {
     public static void main(String[] args) {
         Pessoa teste = new Pessoa();
+
+        teste.setCpf("001.002.003-00");
+        teste.setNome("Clotilde");
+        teste.setDataNascimento("04/08/1700");
         teste.exibirInfo();
     }
 }
