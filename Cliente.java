c8c426e078eadb4e76436fad057b4958794fad57
mayannyda123 <capic.2024118isinf0058@aluno.ifpi.edu.br>
2025-06-12 10:12:15 -0300

    cliente.java criada

---
+++ Cliente.java
@@ -0,0 +1,12 @@
+public class Cliente extends Pessoa {
+    private String cpf;
+
+    public Cliente(String nome, String email, String cpf) {
+        super(nome, email);
+        this.cpf = cpf;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+}
